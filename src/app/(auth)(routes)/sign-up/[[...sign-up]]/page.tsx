c9a7731e7c62231fbe0307c2f9af0749b261ea88
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-08-29 20:52:58 -0700

    sign-in cleanup

--- src/app/(auth)(routes)/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SignUp } from '@clerk/nextjs';
-import * as React from 'react';
-
-
-export default function SignInPage() {
-  return <SignUp />
-}
--- src/app/(routes)/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { SignUp } from '@clerk/nextjs';
-import * as React from 'react';
-
-export default function SignUpPage() {
-  return <SignUp />
-}
