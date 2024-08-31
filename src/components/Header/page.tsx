de4146daeaf66b5b5b3bcbdd8b03c406abc84507
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-08-31 14:18:22 -0700

    header implemted STICKY

--- src/components/Header/page.tsx
+++ src/components/Header/page.tsx
@@ -1,10 +1,69 @@
 import { SignIn } from '@clerk/nextjs'
+import {  clerkClient } from '@clerk/nextjs/server'
+import Link from 'next/link'
 import React from 'react'
+import WidthWrapper from '../ui/Widthwrapper'
 
-export default function Header() {
+
+
+
+
+const Navbar = () => {
+  const  userId  = 'user_123';
+  const response =  clerkClient?.users.getUser(userId);
+  console.log(response);
+  if(userId){
+    console.log(userId);
+  }
   return (
-    <div>Sign in with clerk
-
-    </div>
+      <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/60 backdrop-blur-lg transition-all">
+          <WidthWrapper>
+              <div className="flex h-14 items-center justify-between border-b border-zinc-900">
+                  <Link href="/" className="flex z-40 font-semibold">
+                      <span>WAC</span>
+                      
+                    {/*add logo */}
+                  </Link>
+                  <div className="hidden items-center space-x-4 sm:flex hover:bg-slate-300">
+                      {!userId ? (
+                          <>
+                      
+                      <Link href="/price"className='sm'
+                          // variant: "ghost",
+                          // size: "sm"
+                      >Price
+                      </Link>
+                      <Link href="/sign-in"
+                      className='text:sm'
+                          // variant: "ghost",
+                          // size: "sm",
+                      >
+                          Sign in
+                          <SignIn />
+                      {/* </LoginLink>
+                      <RegisterLink
+                      className={buttonVariants({
+                           */}
+                          {/* size: "sm", */}
+                      {/* })}> */}
+                          Schedule APT
+                          {/* <ArrowRight className='ml-3.5 h-4 w-4' />
+                      </RegisterLink> */}
+                      </Link>
+                      </>
+                      ) : (
+                          <Link 
+                           href='/dashboard'
+                           className='text-sm'>Dashboard</Link>
+                          
+                          
+                           
+                      )}  
+                  </div>
+              </div>
+          </WidthWrapper>
+      </nav>
   )
 }
+
+export default Navbar
