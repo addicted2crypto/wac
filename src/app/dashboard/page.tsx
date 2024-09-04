72fff6fe17200205e1d858fbcdeb60d37a2bf269
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-09-03 19:26:29 -0700

    header cleanup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,12 +9,12 @@ export default function Dashboard() {
   return (
     <div className='flex items-center justify-center bg-[#fff]'>
       <div className='text-sm '>Dashboard for all logged in users
-        <div className='text-lg absolute left-[5rem]'>Welcome to your applaince solutions page!</div>
+        <div className='text-lg absolute left-[3rem] overflow-auto'>Welcome to your applaince solutions page!</div>
         <div className='pt-8 w-max'>
         <textarea placeholder='Submit appliance Errors and problems here 👇' className='w-[23rem] '></textarea>
         </div>
       </div>
-      <div className='absolute right-10 top-10'>
+      <div className='absolute right-6 top-6'>
         <div className='text-[#000]'>{user?.fullName}</div>
       
       </div>
