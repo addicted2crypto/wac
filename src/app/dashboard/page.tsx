d8bd747d710ba9ade98f497b3f7a90129ffc7d07
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-09-07 23:08:44 -0700

    user logged in only

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,12 +3,17 @@ import { SignIn, SignOutButton, UserButton } from '@clerk/nextjs';
 import { useUser } from '@clerk/nextjs';
 import { User } from '@clerk/nextjs/dist/types/server';
 import React from 'react';
-import Upload from './upload/page';
+
 
 export default function Dashboard() {
   const { isSignedIn, user, isLoaded } = useUser();
-
+  
+  {user? console.log(user.emailAddresses) : console.log('no user email found');
+  {isSignedIn? console.log('thank you for signing in'): console.log('Please sign in')};
+  
+  
   return (
+    
     <div className='flex items-center justify-center bg-[#fff]'>
       <div className='text-sm '>Dashboard for all logged in users
         <div className='text-2xl text-sky-950 absolute left-[3rem] overflow-auto'>Welcome to your applaince solutions page!</div>
@@ -17,7 +22,7 @@ export default function Dashboard() {
         </div>
       </div>
       <div className='absolute right-8 top-10'>
-        <div className='text-[#246e2b]'>Over 100 years of appliance repair experience at your finger tips</div>
+        <div className='text-[#246e2b] font-serif'>Over 100 years of appliance repair experience at your finger tips</div>
      
       </div>
       <div className='absolute right-2 top-2 overflow-auto'>
@@ -27,5 +32,7 @@ export default function Dashboard() {
       
     </div>
   )
+  
+  }
 }
 
