85dc0167640cb1b24bca1aed47d1c4ec4f98e0ea
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-09-21 19:53:33 -0700

    env to gitignore!

--- src/app/api/submit-issue.ts
+++ src/app/api/submit-issue.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getAuth } from '@clerk/nextjs/server';
 import { UTApi } from 'uploadthing/server';
 import { MongoClient } from 'mongodb';
-import { NextResponse } from 'next/server';
+
 
 const uri = process.env.MONGODB_URI;
 const utapi = new UTApi();
@@ -11,8 +11,8 @@ if(!uri){
   throw new Error( 'Please add your MongoDB uri to .env.local');
 }
 
-
-let client: MongoClient | null = null;
+const client = new MongoClient(uri);
+// let client: MongoClient | null = null;
 
 // if(!client) {
 //   client = new MongoClient(uri);
@@ -24,72 +24,82 @@ interface SubmitIssueRequestBody {
     name: string;
     type: string;
     size: number;
-    data: ArrayBuffer
+    url: string;
   }[]; 
 }
 
-export  async function POST(req: NextApiRequest, res: NextApiResponse) {
+export  async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
 
   try {
-    await client?.connect();
+    await client.connect();
     console.log('Connected to MongoDB');
     const { userId } = getAuth(req);
     console.log('Extracted userId:', userId);
-
+      
     if (!userId) {
       return res.status(401).json({ message: 'Unauthorized' });
     }
 
     const { textContent, files } = await req.body as SubmitIssueRequestBody;
+    console.log('Request body: ', { textContent, files });
+    // if (!client) {
+    //   client = new MongoClient(uri as string);
+    //   await client.connect();
+    // }
 
-    if (!client) {
-      client = new MongoClient(uri as string);
-      await client.connect();
-    }
+    const db = client.db('ImCounsulting');  /* db name */
 
-    const db = client.db('Appliance_Consult');
-
-    const issues = db.collection('Consult');
+    const issues = db.collection('ApplianceDocs');  /* collection name */
 
     let fileUrls: string[] = [];
 
    
 
     if (files && Array.isArray(files) && files.length > 0 ) {
-      const uploadPromises = files.map(async (file) => {
-      try {
-        const fileBob = new Blob([file.data], { type: file.type});
-        const fileEsque = new File([fileBob], file.name, { type: file.type})
+      fileUrls = files.map(file => file.url).filter(Boolean) 
+    }
+      
+      // try {
+      //   const fileBob = new Blob([file.data], { type: file.type});
+      //   const fileEsque = new File([fileBob], file.name, { type: file.type})
 
-        const uploadedFile = await utapi.uploadFiles([fileEsque]);
+      //   const uploadedFile = await utapi.uploadFiles([fileEsque]);
         
         // if (uploadedFile.error ){
         //   console.error('File upload failed: No URL returned');
         //   return null;
 
         // }
-        return uploadedFile[0].data?.url || null;
-      } catch (error) {
-        console.error('Error uploading file:', error);
-        return null;
-      }
-    });
+        const result = await issues.insertOne({
+          userId,
+          textContent,
+          fileUrls,
+          createdAt: new Date()
+        });
 
-    const uploadResults = await Promise.all(uploadPromises);
-    fileUrls = uploadResults.filter((url): url is string => url !== null);
-  }
+        console.log('Saved to MongoDb', result.insertedId);
+      //   return uploadedFile[0].data?.url || null;
+      // } catch (error) {
+      //   console.error('Error uploading file:', error);
+      //   return null;
+      // }
+    // });
 
-  const result = await issues.insertOne({
-    userId,
-    textContent,
-    fileUrls,
-    createdAt: new Date()
-  });
+  //   const uploadResults = await Promise.all(uploadPromises);
+  //   fileUrls = uploadResults.filter((url): url is string => url !== null);
+  // }
 
-  console.log('Saved to MongoDB:', result.insertedId);
+  // const result = await issues.insertOne({
+  //   userId,
+  //   textContent,
+  //   fileUrls,
+  //   createdAt: new Date()
+  // });
+
+  // console.log('Saved to MongoDB:', result.insertedId);
 
     // res.status(200).json({ message: 'Issue submitted successfully' });
     return res.status(200).json({ message: 'Issue submitted successfully'})
