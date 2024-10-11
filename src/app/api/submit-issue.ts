c3a0a0148d8a41b2f0373c4cd132ac2fac9706b0
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-10-10 20:03:21 -0700

    response export updates

--- src/app/api/submit-issue.ts
+++ src/app/api/submit-issue.ts
@@ -30,6 +30,9 @@ interface SubmitIssueRequestBody {
 }
 
 export  async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
