e3a85a6cb62cd0a451db4f267797df7efbbfd2ef
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-11-02 18:14:38 -0700

    adding ability to upload videos!

--- src/app/api/uploadthing.ts
+++ src/app/api/uploadthing.ts
@@ -1,4 +1,4 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { createUploadthing, type FileRouter } from "uploadthing/server";
  
 const f = createUploadthing();
  
@@ -6,8 +6,14 @@ export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     .onUploadComplete(async ({ metadata, file }) => {
       console.log("Upload complete for userId:", metadata);
-      console.log("file url", file.url);
+      console.log("file url", file.url)
     }),
+    uploadedApplianceData: f(["image", "video"])
+    // add find a way to protect with auth
+    // .middleware(({ req }) => auth(req))
+    .onUploadComplete((data) => console.log("file", data)),
+
+    
 } 
  
 export type OurFileRouter = typeof ourFileRouter;
